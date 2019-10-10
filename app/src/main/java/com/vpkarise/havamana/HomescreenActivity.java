8d97487bb04c54150ee5c66008f38cd35bf70e6c
Vishwas poojary k <vishwasvogga@gmail.com>
2019-10-10 22:25:12 +0530

    initial commit and value files updated

---
+++ app/src/main/java/com/vpkarise/havamana/HomescreenActivity.java
@@ -0,0 +1,28 @@
+package com.vpkarise.havamana;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Home screen activity where we search the cities
+ */
+public class HomescreenActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_homescreen);
+
+    }
+
+
+}
